312a149c7a30ea6074cf2dd87fe3573c3f6445ce
arunangshunayak <arunangshu17@gmail.com>
2023-08-07 22:19:28 +0530

    Adding all Files

---
+++ AWS/TF_Files/variable.tf
@@ -0,0 +1,14 @@
+variable "AWSAccessKeyId" {
+  type    = string
+  default = "RemovedAfterUse"
+}
+variable "AWSSecretKey" {
+  type    = string
+  default = "RemovedAfterUse"
+}
+
+variable "AMI" {
+  type    = string
+  default = "ami-03f65b8614a860c29"
+}
+
